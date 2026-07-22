6d6ee36e5432b6fb0a9d7525c1d0799994ea83c8
Sebastian Wolschon <sebastian.wolschon@googlemail.com>
2026-07-22 17:55:34 +0200

    React to Device chain color changes
    Note: Color mapping isn't perfect and the Morningstar color chart seems wildly wrong aswell.

--- MC8_Pro.py
+++ MC8_Pro.py
@@ -8,6 +8,8 @@ from _Framework.ButtonElement import ButtonElement
 from _Framework.InputControlElement import MIDI_CC_TYPE
 from _Framework.TransportComponent import TransportComponent
 from ableton.v2.base import Slot, SlotGroup
+from ableton.v2.base import listens_group
+
 import logging
 
 from .SysExMessenger import SysExMessenger
@@ -22,8 +24,10 @@ class MC8_Pro(ControlSurface):
 
     def _build(self):
 
-        BUTTON_CC_NUMBERS = [58, 59, 60, 61] # Select between the first 4 chains in the first rack of the current track
-        MIDI_CHANNEL = 0 # Channel 1 in Ableton is index 0 in Python
+        self._messenger = SysExMessenger(self._send_midi)
+
+        self.BUTTON_CC_NUMBERS = [58, 59, 60, 61] # Select between the first 4 chains in the first rack of the current track
+        self.MIDI_CHANNEL = 0 # Channel 1 in Ableton is index 0 in Python
 
         transport = TransportComponent()
 
@@ -35,10 +39,13 @@ class MC8_Pro(ControlSurface):
         transport.set_stop_button(stop_button)
         transport.set_record_button(record_button)
 
-        self.buttons = []
-        self._macro_listener_target = None
+        self._chain_select_buttons = self._create_chain_select_buttons()
+
+        self._macro_listener_slot = Slot(listener=self._update_led_feedback, event_name="value")
 
         self._chain_name_slots = SlotGroup(listener=self._on_chain_name_changed, event_name="name")
+        self._chain_color_slots = SlotGroup(listener=self._on_chain_color_changed, event_name="color")
+        self._on_chain_selector_value.replace_subjects(self._chain_select_buttons)
 
         self._rack_chains_slot = Slot(
             listener=self._on_rack_structure_changed,
@@ -51,14 +58,15 @@ class MC8_Pro(ControlSurface):
         event_name="selected_track"     # Looks for 'selected_track_has_listener'
         )
 
-        self._messenger = SysExMessenger(self._send_midi)
-
-        for index, cc in enumerate(BUTTON_CC_NUMBERS):
-            btn = DynamicFeedbackButton(True, MIDI_CC_TYPE, MIDI_CHANNEL, cc,index)
-            btn.add_value_listener(self._on_button_pressed, identify_sender=True)
-            self.buttons.append(btn)
-
         self._on_track_changed()
+
+    def _create_chain_select_buttons(self):
+        buttons = []
+        for feedback_val, cc in enumerate(self.BUTTON_CC_NUMBERS):
+            btn = DynamicFeedbackButton(True, MIDI_CC_TYPE, self.MIDI_CHANNEL, cc, feedback_val)
+            buttons.append(btn)
+
+        return buttons
 
     def _get_first_rack_device(self,track):
         """Helper to find the first valid rack device on the track"""
@@ -71,7 +79,6 @@ class MC8_Pro(ControlSurface):
 
         # logger.info('Track Changed')
         """Triggered automatically when switching tracks"""
-        self._cleanup_listeners()
         
         track = self.song().view.selected_track
         device = self._get_first_rack_device(track)
@@ -79,14 +86,15 @@ class MC8_Pro(ControlSurface):
         if device is not None:
             # 1. Listen to macro value changes for LEDs
             if len(device.parameters) > 1:
-                self._macro_listener_target = device.parameters[1]
-                self._macro_listener_target.add_value_listener(self._update_led_feedback)
-            
+                self._macro_listener_slot.subject = device.parameters[1]
+            else:
+                self._macro_listener_slot.subject = None
             # 2. Listen to structural rack changes (Add, Delete, Move chains)
             self._rack_chains_slot.subject = device
             
             # 3. Setup individual text change listeners for current chains
             self._setup_chain_name_listeners(device.chains)
+            self._setup_chain_color_listeners(device.chains)
         else:
             self._rack_chains_slot.subject = None
 
@@ -101,6 +109,15 @@ class MC8_Pro(ControlSurface):
         # and safely binds the 'name' listener to the new 1-4 chains.
         self._chain_name_slots.replace_subjects(target_chains)
 
+    def _setup_chain_color_listeners(self, chains):
+        """Attaches rename listeners to up to the first 4 chains safely using SlotGroup"""
+        # Slice the first 4 chains
+        target_chains = chains[:4]
+        
+        # replace_subjects automatically removes old listeners, ignores deleted/null objects, 
+        # and safely binds the 'name' listener to the new 1-4 chains.
+        self._chain_color_slots.replace_subjects(target_chains)
+
     def _on_rack_structure_changed(self, *args):
 
         # logger.error('Rack Structure Changed')
@@ -110,17 +127,24 @@ class MC8_Pro(ControlSurface):
         if device is not None:
             # Pass the new chain list. SlotGroup safely cleans up the old, dead chains.
             self._setup_chain_name_listeners(device.chains)
+            self._setup_chain_color_listeners(device.chains)
         else:
             # If the rack itself was deleted, completely clear all tracked chain slots safely
             self._chain_name_slots.replace_subjects([])
+            self._chain_color_slots.replace_subjects([])
 
         # Force structural recalculation updates down to the hardware
         self._update_all_button_names()
+        self._update_all_button_leds()
         self._update_led_feedback()
 
     def _on_chain_name_changed(self, chain):
         """Triggered when a specific visible chain is explicitly renamed"""
         self._update_all_button_names()
+
+    def _on_chain_color_changed(self, chain):
+        # logger.info('Chain Color Changed')
+        self._update_all_button_leds()
 
     def _update_all_button_names(self):
         """Updates text fields or blanks them if chains were deleted"""
@@ -133,7 +157,21 @@ class MC8_Pro(ControlSurface):
             else:
                 self._messenger.send_name_sysex(index, "[EMPTY]")
 
-    def _on_button_pressed(self, value, sender):
+    def _update_all_button_leds(self):
+        """Updates colors blanks them if chains were deleted"""
+        device = self._rack_chains_slot.subject
+        chains = device.chains if device is not None else []
+        # logger.info('Updating chain colors.')
+        for index in range(4):
+            if index < len(chains):
+                self._messenger.send_color_sysex(index, chains[index].color_index)
+                # logger.info(f"New Color index: {chains[index].color_index}")
+            else:
+                self._messenger.send_color_sysex(index, 0)
+
+    @listens_group('value')
+    def _on_chain_selector_value(self, value, button):
+
         if value == 0:
             return
 
@@ -141,12 +179,12 @@ class MC8_Pro(ControlSurface):
         if device is None or len(device.parameters) <= 1:
             return
 
-        macro_1 = device.parameters[1]
+        macro_1 = self._macro_listener_slot.subject
         total_chains = len(device.chains)
         if total_chains == 0:
             return
 
-        btn_index = self.buttons.index(sender)
+        btn_index = self._chain_select_buttons.index(button)
         if btn_index >= total_chains:
             return
 
@@ -154,28 +192,23 @@ class MC8_Pro(ControlSurface):
         target_value = int((btn_index * step_size) + (step_size / 2))
         macro_1.value = target_value
 
-    def _update_led_feedback(self):
+    def _update_led_feedback(self,):
         device = self._rack_chains_slot.subject
         # logger.info(f"Device name is: {device.name if device else 'NO DEVICE'}")
-        if device is None or len(device.parameters) <= 1:
-            for btn in self.buttons:
+        if device is None or len(device.parameters) <= 1 or len(device.chains) <=0:
+            for btn in self._chain_select_buttons:
                 btn.send_value(0)
             return
 
         macro_1 = device.parameters[1]
         total_chains = len(device.chains)
 
-        if total_chains == 0:
-            for btn in self.buttons:
-                btn.send_value(0)
-            return
-
         step_size = 128.0 / total_chains
         active_index = int(macro_1.value / step_size)
         if active_index >= total_chains:
             active_index = total_chains - 1
 
-        for index, btn in enumerate(self.buttons):
+        for index, btn in enumerate(self._chain_select_buttons):
             if index == active_index and index < total_chains:
                 btn.send_value(127) 
             else:
@@ -184,17 +217,14 @@ class MC8_Pro(ControlSurface):
     def handle_sysex(self, midi_bytes):
         self._messenger.parse_incoming_sysex(midi_bytes)
 
-    def _cleanup_listeners(self):
-        """Helper to break connections cleanly and avoid leaking pointers"""
-        if self._macro_listener_target is not None:
-            if self._macro_listener_target.value_has_listener(self._update_led_feedback):
-                self._macro_listener_target.remove_value_listener(self._update_led_feedback)
-            self._macro_listener_target = None
-
     def disconnect(self):
-        self._cleanup_listeners()
+        self._macro_listener_slot.disconnect()
         self._chain_name_slots.disconnect()
+        self._chain_color_slots.disconnect()
         self._rack_chains_slot.disconnect()
+        for slot in self._chain_select_buttons:
+            slot.disconnect()
+
         super(MC8_Pro, self).disconnect()
 
     def send_morningstar_midi(self, status, target_cc, feedback_val):
--- SysExMessenger.py
+++ SysExMessenger.py
@@ -1,4 +1,5 @@
 import logging
+import math
 
 logger = logging.getLogger(__name__)
 
@@ -10,6 +11,172 @@ class SysExMessenger(object):
         0x02: "WRONG CHECKSUM",
         0x03: "WRONG PAYLOAD SIZE"
     }
+
+    SYSEX_HEADER = [
+            0xF0,  # SysEx Start
+            0x00,  # Manufacturer ID 1
+            0x21,  # Manufacturer ID 2
+            0x24,  # Manufacturer ID 3
+            0x08,  # Device Model ID
+            0x00,  # Ignore
+            0x70,  # Opcode 1
+    ]
+
+    OPCODES = {
+        'send_short_name': 0x01,
+        "send_toggle_name": 0x02,
+        "send_long_name": 0x03,
+        "send_message": 0x04,
+        "send_other_data": 0x05
+    }
+
+    ABLETON_TO_MC8_COLORS = {
+        0: [59, 123],     #"LIGHT_PINK"
+        1:[51, 115] ,     #"LIGHT_SALMON"
+        2: [47, 111],     #"GOLD"
+        3: [50, 114],     #"LIGHT_YELLOW"
+        4: [12, 26],     #"PALE_GREEN"
+        5: [29, 93],     #"AQUA"
+        6: [10, 24],     #"LIGHT_BLUE"
+        7: [33, 97],     #"LIGHT_STEEL_BLUE"
+        8:[45, 109] ,     #"THISTLE"
+        9: [38, 102],     #"LAVENDER"
+        10: [56, 120],    # "SALMON"
+        11: [52, 116],    # "TOMATO"
+        12:[48, 112] ,    # "KHAKI"
+        13: [7, 21],    # "KHAKI"
+        14: [1, 15],    # "GREEN"
+        15: [30, 94],    # "TURQUOISE"
+        16:[31, 95] ,    # "CADET_BLUE"
+        17: [32, 96],    # "STEEL_BLUE"
+        18: [13, 27],    # "PALE_PURPLE"
+        19: [61, 125],    # "PALE_VIOLET_RED"
+        20:[60, 124] ,    # "HOT_PINK"
+        21: [53, 117],    # "ORANGE_RED"
+        22: [34, 98],    # "TAN"
+        23: [4, 18],    # "YELLOW"
+        24: [1, 15],    # "GREEN"
+        25: [39, 103],    # "TEAL"
+        26:[2, 16] ,    # "BLUE"
+        27:[2, 16] ,    # "BLUE"
+        28: [44, 108],    # "BLUE_VIOLET"
+        29: [5, 19],    # "PINK"
+        30: [9, 23],    # "RED"
+        31: [54, 118],    # "DARK_ORANGE"
+        32: [35, 99],    # "BROWN"
+        33:[4, 18] ,    # "YELLOW"
+        34: [1, 15],    # "GREEN"
+        35: [39, 103],    # "TEAL"
+        36: [2, 16],    # "BLUE"
+        37: [2, 16],    # "BLUE"
+        38:[3, 17] ,    # "PURPLE"
+        39: [11, 25],    # "DARK_PINK"
+        40: [55, 119],    # "INDIAN_RED"
+        41: [8, 22],    # "ORANGE"
+        42: [49, 113],    # "DARK_KHAKI"
+        43: [42, 106],    # "OLIVE"
+        44: [40, 104],    # "DARK_SEA_GREEN"
+        45: [39, 103],    # "TEAL"
+        46: [31, 95],    # "CADET_BLUE"
+        47:[32, 96] ,    # "STEEL_BLUE"
+        48: [46, 110],    # "REBECCA_PURPLE"
+        49: [3, 17],    # "PURPLE"
+        50: [57, 121],    # "FIRE_BRICK"
+        51: [37, 101],    # "MAROON"
+        52: [35, 99],    # "BROWN"
+        53: [42, 106],    # "OLIVE"
+        54: [41, 105],    # "FOREST_GREEN"
+        55: [39, 103],    # "TEAL"
+        56: [31, 95],    # "CADET_BLUE"
+        57: [43, 107],    # "INDIGO"
+        58: [43, 107],    # "INDIGO"
+        59: [3, 17],    # "PURPLE"
+        60:[58, 122] ,    # "DARK_RED"
+        61: [37, 101],    # "MAROON"
+        62: [35, 99],    # "BROWN"
+        63: [42, 106],    # "OLIVE"
+        64: [14, 28],    # "DARK_GREEN"
+        65: [39, 103],    # "TEAL"
+        66:[7, 21] ,    # "WHITE"
+        67: [62, 126],    # "LIGHT_GRAY"
+        68: [63, 127],    # "SLATE_GRAY"
+        69: [0, 64],    # "BLACK
+    }
+
+    # Complete Ableton Live 70-Color Mapping with Hex, RGB tuples, and LOM Integers
+    ABLETON_COLOR_PALETTE = [
+        {'id':0,"hex": "#FF9999", "rgb": (255, 153, 153), "lom_int": 16751001},
+        {'id':1,"hex": "#FFB399", "rgb": (255, 179, 153), "lom_int": 16757657},
+        {'id':2,"hex": "#FFCC99", "rgb": (255, 204, 153), "lom_int": 16764057},
+        {'id':3,"hex": "#FFE699", "rgb": (255, 230, 153), "lom_int": 16770713},
+        {'id':4,"hex": "#FFFF99", "rgb": (255, 255, 153), "lom_int": 16777113},
+        {'id':5,"hex": "#E6FF99", "rgb": (230, 255, 153), "lom_int": 15138713},
+        {'id':6,"hex": "#CCFF99", "rgb": (204, 255, 153), "lom_int": 13434777},
+        {'id':7,"hex": "#B3FF99", "rgb": (179, 255, 153), "lom_int": 11796377},
+        {'id':8,"hex": "#99FF99", "rgb": (153, 255, 153), "lom_int": 10092441},
+        {'id':9,"hex": "#99FFA6", "rgb": (153, 255, 166), "lom_int": 10092454},
+        {'id':10,"hex": "#99FFB3", "rgb": (153, 255, 179), "lom_int": 10092467},
+        {'id':11,"hex": "#99FFCC", "rgb": (153, 255, 204), "lom_int": 10092492},
+        {'id':12,"hex": "#99FFE6", "rgb": (153, 255, 230), "lom_int": 10092518},
+        {'id':13,"hex": "#99FFFF", "rgb": (153, 255, 255), "lom_int": 10092543},
+        {'id':14,"hex": "#E6FFFF", "rgb": (230, 255, 255), "lom_int": 15138815},
+        {'id':15,"hex": "#CCFFFF", "rgb": (204, 255, 255), "lom_int": 13434879},
+        {'id':16,"hex": "#B3FFFF", "rgb": (179, 255, 255), "lom_int": 11796479},
+        {'id':17,"hex": "#99FFFF", "rgb": (153, 255, 255), "lom_int": 10092543},
+        {'id':18,"hex": "#99E6FF", "rgb": (153, 230, 255), "lom_int": 10086143},
+        {'id':19,"hex": "#99CCFF", "rgb": (153, 204, 255), "lom_int": 10079743},
+        {'id':20,"hex": "#99B3FF", "rgb": (153, 179, 255), "lom_int": 10073343},
+        {'id':21,"hex": "#9999FF", "rgb": (153, 153, 255), "lom_int": 10066431},
+        {'id':22,"hex": "#B399FF", "rgb": (179, 153, 255), "lom_int": 11770367},
+        {'id':23,"hex": "#CC99FF", "rgb": (204, 153, 255), "lom_int": 13408767},
+        {'id':24,"hex": "#E699FF", "rgb": (230, 153, 255), "lom_int": 15106559},
+        {'id':25,"hex": "#FF99FF", "rgb": (255, 153, 255), "lom_int": 16751103},
+        {'id':26,"hex": "#FF99E6", "rgb": (255, 153, 230), "lom_int": 16751078},
+        {'id':27,"hex": "#FF99CC", "rgb": (255, 153, 204), "lom_int": 16751052},
+        {'id':28,"hex": "#FF99B3", "rgb": (255, 153, 179), "lom_int": 16751027},
+        {'id':29,"hex": "#FF4D4D", "rgb": (255, 77, 77),   "lom_int": 16731469},
+        {'id':30,"hex": "#FF734D", "rgb": (255, 115, 77),  "lom_int": 16741197},
+        {'id':31,"hex": "#FF994D", "rgb": (255, 153, 47),  "lom_int": 16751181},
+        {'id':32,"hex": "#FFBF4D", "rgb": (255, 191, 47),  "lom_int": 16760909},
+        {'id':33,"hex": "#FFE64D", "rgb": (255, 230, 77),  "lom_int": 16770637},
+        {'id':34,"hex": "#D9FF4D", "rgb": (217, 255, 77),  "lom_int": 14286669},
+        {'id':35,"hex": "#B3FF4D", "rgb": (179, 255, 77),  "lom_int": 11796301},
+        {'id':36,"hex": "#8CFF4D", "rgb": (140, 255, 77),  "lom_int": 9240397},
+        {'id':37,"hex": "#66FF4D", "rgb": (102, 255, 77),  "lom_int": 6749965},
+        {'id':38,"hex": "#4DFF4D", "rgb": (77, 255, 77),   "lom_int": 5111629},
+        {'id':39,"hex": "#4DFF73", "rgb": (77, 255, 115),  "lom_int": 5111667},
+        {'id':40,"hex": "#4DFF99", "rgb": (77, 255, 153),  "lom_int": 5111705},
+        {'id':41,"hex": "#4DFFBF", "rgb": (77, 255, 191),  "lom_int": 5111743},
+        {'id':42,"hex": "#4DFFE6", "rgb": (77, 255, 230),  "lom_int": 5111782},
+        {'id':43,"hex": "#4DFFFF", "rgb": (77, 255, 255),  "lom_int": 5111807},
+        {'id':44,"hex": "#4DE6FF", "rgb": (77, 230, 255),  "lom_int": 5105407},
+        {'id':45,"hex": "#4DC2FF", "rgb": (77, 194, 255),  "lom_int": 5096191},
+        {'id':46,"hex": "#4D99FF", "rgb": (77, 153, 255),  "lom_int": 5085695},
+        {'id':47,"hex": "#4D73FF", "rgb": (77, 115, 255),  "lom_int": 5075967},
+        {'id':48,"hex": "#4D4DFF", "rgb": (77, 77, 255),   "lom_int": 5066239},
+        {'id':49,"hex": "#734DFF", "rgb": (115, 77, 255),  "lom_int": 7556095},
+        {'id':50,"hex": "#994DFF", "rgb": (153, 77, 255),  "lom_int": 10046463},
+        {'id':51,"hex": "#BF4DFF", "rgb": (191, 77, 255),  "lom_int": 12536831},
+        {'id':52,"hex": "#E64DFF", "rgb": (230, 77, 255),  "lom_int": 15093247},
+        {'id':53,"hex": "#FF4DFF", "rgb": (255, 77, 255),  "lom_int": 16731391},
+        {'id':54,"hex": "#FF4DE6", "rgb": (255, 77, 230),  "lom_int": 16731366},
+        {'id':55,"hex": "#FF4DC2", "rgb": (255, 77, 194),  "lom_int": 16731330},
+        {'id':56,"hex": "#FF4D99", "rgb": (255, 77, 153),  "lom_int": 16731289},
+        {'id':57,"hex": "#FF4D73", "rgb": (255, 77, 115),  "lom_int": 16731251},
+        {'id':58,"hex": "#FF0000", "rgb": (255, 0, 0),     "lom_int": 16711680},
+        {'id':59,"hex": "#FF3300", "rgb": (255, 51, 0),    "lom_int": 16724736},
+        {'id':60,"hex": "#FF6600", "rgb": (255, 102, 0),   "lom_int": 16737792},
+        {'id':61,"hex": "#FF9900", "rgb": (255, 153, 0),   "lom_int": 16750848},
+        {'id':62,"hex": "#FFCC00", "rgb": (255, 204, 0),   "lom_int": 16763904},
+        {'id':63,"hex": "#FFFF00", "rgb": (255, 255, 0),   "lom_int": 16776960},
+        {'id':64,"hex": "#CCFF00", "rgb": (204, 255, 0),   "lom_int": 13434624},
+        {'id':65,"hex": "#99FF00", "rgb": (153, 255, 0),   "lom_int": 10092288},
+        {'id':66,"hex": "#FFFFFF", "rgb": (255, 255, 255), "lom_int": 16777215},
+        {'id':67,"hex": "#CCCCCC", "rgb": (204, 204, 204), "lom_int": 13421772},
+        {'id':68,"hex": "#808080", "rgb": (128, 128, 128), "lom_int": 8421504},
+        {'id':69,"hex": "#000000", "rgb": (0, 0, 0),       "lom_int": 0}
+    ]
+    
         
     def __init__(self, send_midi_callback):
 
@@ -37,14 +204,8 @@ class SysExMessenger(object):
 
         # Build Fixed Header
         sysex_bytes = [
-            0xF0,  # SysEx Start
-            0x00,  # Manufacturer ID 1
-            0x21,  # Manufacturer ID 2
-            0x24,  # Manufacturer ID 3
-            0x08,  # Device Model ID
-            0x00,  # Ignore
-            0x70,  # Opcode 1
-            0x01,  # Opcode 2 - Update Preset Short Name
+            *self.SYSEX_HEADER,
+            self.OPCODES['send_short_name'],
             button_index & 0x7F, # Opcode 3 - Button index
             0x00,  # Opcode 4
             0x00,  # Opcode 5
@@ -61,6 +222,55 @@ class SysExMessenger(object):
         # Convert the fixed 32-character string to ASCII bytes
         ascii_payload = [ord(char) & 0x7F for char in padded_name]
         sysex_bytes.extend(ascii_payload)
+
+        # Calculate checksum using the XOR algorithm on the current array
+        checksum_byte = self._calculate_checksum(sysex_bytes)
+        sysex_bytes.append(checksum_byte)
+
+        # Append End of SysEx
+        sysex_bytes.append(0xF7)
+
+        # Deliver packet to hardware via the provided framework callback
+        self._send_midi(tuple(sysex_bytes))
+
+    def send_color_sysex(self, button_index, color):
+        """Constructs and delivers the explicit padded SysEx packet format"""
+        # Increment transaction ID (0-127 MIDI legal limit)
+        self._transaction_id = (self._transaction_id + 1) & 0x7F
+
+
+
+        hexNumberDim = int(self.ABLETON_TO_MC8_COLORS[color][1]) 
+        hexNumberFull = int(self.ABLETON_TO_MC8_COLORS[color][0])
+
+        # Build Fixed Header
+        sysex_bytes = [
+            *self.SYSEX_HEADER,
+            self.OPCODES['send_other_data'],
+            button_index & 0x7F, # Opcode 3 - Button index
+            0x00,  # Opcode 4
+            0x00,  # Opcode 5
+            0x00,  # Opcode 6
+            0x00,  # Opcode 7
+            self._transaction_id, # Transaction ID
+            0x00,  # Ignore
+            0x00,   # Ignore
+            0x01,  # Preset Toggle: 7F = ON, 00 = OFF, Other = IGNORE
+            0x01,  # Preset Blink: 7F = ON, 00 = OFF, Other = IGNORE
+            0x01,  # Preset Message Scroll Mode: 7F = ON, 00 = OFF, Other = IGNORE
+            0x11,  # Preset Global Toggle Group: 00 = Independent, 01-10 = Groups 1 to 16, Other = IGNORE
+            hexNumberDim,  # Pos 1 LED Color        (PRO only) F7 = IGNORE
+            hexNumberFull,  # Pos 2 LED Color        (PRO only) F7 = IGNORE
+            0x7F,     # Shift LED Color        (PRO only) F7 = IGNORE
+            0x7F,     # Pos 1 Text Color       (PRO only) F7 = IGNORE
+            0x7F,     # Pos 2 Text Color       (PRO only) F7 = IGNORE
+            0x7F,     # Shift Text Color       (PRO only) F7 = IGNORE
+            0x7F,     # Pos 1 Background Color (PRO only) F7 = IGNORE
+            0x7F,     # Pos 2 Background Color (PRO only) F7 = IGNORE
+            0x7F     # Shift Background Color (PRO only) F7 = IGNORE
+        ]
+
+        # logger.info(sysex_bytes)
 
         # Calculate checksum using the XOR algorithm on the current array
         checksum_byte = self._calculate_checksum(sysex_bytes)
@@ -116,3 +326,4 @@ class SysExMessenger(object):
             # Optional: Uncomment if you want to track successful handshakes while debugging
             # Live.Base.log(f"[Hardware Success] Transaction ID {transaction_id} acknowledged successfully.")
             pass
+
